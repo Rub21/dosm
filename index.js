c09054785dcf311de40eec37acff3d54574f1c7e
Rub21 <rub2106@gmail.com>
2019-02-13 10:25:21 -0500

    Download tiles

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const program = require('commander');
 const argv = require('minimist')(process.argv.slice(2));
 const download = require('./lib/download');
+const downloadTiles = require('./lib/downloadTiles');
+
 program
   .version('0.0.1')
   .option('-d, --download', 'Download data from overpass')
+  .option('-t, --downloadTiles', 'Download Tiles')
   .parse(process.argv);
 
 const file = process.argv.slice(2)[1];
@@ -15,4 +18,8 @@ if (program.download) {
     zoom: argv.zoom
   };
   download(file, opts);
+}
+
+if (program.downloadTiles) {
+  downloadTiles(file, argv.zoom);
 }
